b6f18f8b940c946f70996c8f77b78b003f0cf671
ltom <tom.liere@ynov.com>
2023-02-27 02:09:25 +0100

    start site final

---
+++ Web/Exercices/Site Final/js/navbar.js
@@ -0,0 +1,38 @@
+let alreadyReloaded = false;
+
+function verifierMedia() {
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    const wrap = document.querySelector('.wrapper nav');
+    const lien1 = document.getElementById('link-1');
+    const lien2 = document.getElementById('link-2');
+    const lien3 = document.getElementById('link-3');
+    const lien4 = document.getElementById('link-4');
+    const btn = document.querySelector('.btn-img');
+    if (!alreadyReloaded) {
+      alreadyReloaded = true;
+      btn.addEventListener("click", function(){
+        if (wrap.style.height == "70px"){
+          wrap.style.height = "80vh";
+          lien1.style.opacity = "1";
+          lien2.style.opacity = "1";
+          lien3.style.opacity = "1";
+          lien4.style.opacity = "1";
+        } else {
+          wrap.style.height = "70px";
+          lien1.style.opacity = "0";
+          lien2.style.opacity = "0";
+          lien3.style.opacity = "0";
+          lien4.style.opacity = "0";
+        }
+      })
+    }
+  } else {
+    if (window.matchMedia("(min-width: 900px)").matches) {
+       refresh()
+    }
+  }
+}
+
+window.addEventListener('resize', verifierMedia);
+
+verifierMedia();
